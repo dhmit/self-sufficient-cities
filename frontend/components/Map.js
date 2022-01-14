dc8d51c648e5e3b70538bcfd8e6009817ee50d44
JusticeV452 <jmvidal@mit.edu>
2022-01-14 17:02:01 -0500

    Updated map to be more dynamic
    - Map uses testdata to render pointers
    - Drew polygon on the map and made polygon clickable

--- frontend/components/Map.js
+++ frontend/components/Map.js
@@ -1,5 +1,26 @@
-import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import {MapContainer, Marker, Popup, TileLayer, Polygon} from "react-leaflet";
 import React from "react";
+
+const TESTDATA = [{
+    location: "Test title",
+    date: "Test date",
+    info: "Test info",
+    position: [38.8051606, -77.0036513],
+}];
+
+const MAINLOCATION = {
+    position: [38.9051606, -77.0036513],
+    location: "Deanwood neighborhood, Washington DC",
+    date: "Test date",
+    info: "Test info",
+};
+
+const TESTREGION = [
+    [38.8151606, -77.0036513],
+    [38.8941606, -77.0036513],
+    [38.8051606, -77.0136513],
+    [38.8051606, -77.0006513],
+];
 
 export default class Map extends React.Component {
     state = {
@@ -8,18 +29,28 @@ export default class Map extends React.Component {
     }
 
     render() {
+        // REMOVE THIS FROM RENDER
+        TESTDATA.push(MAINLOCATION);
+        const markerObjects = TESTDATA.map((x, i) => (
+            <Marker key={i} position={x.position}>
+                <Popup>
+                    A pretty CSS3 popup. <br/> Easily customizable.
+                </Popup>
+            </Marker>
+        ));
         return <div id="map">
             <h1>{this.state.location}</h1>
-            <MapContainer center={this.state.position} zoom={13} scrollWheelZoom={false}>
+            <MapContainer center={this.state.position} zoom={13} scrollWheelZoom={true}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={this.state.position}>
-                    <Popup>
-                        A pretty CSS3 popup. <br/> Easily customizable.
-                    </Popup>
-                </Marker>
+                {markerObjects}
+                <Polygon eventHandlers={{
+                    click: () => {
+                        console.log('marker clicked');
+                    },
+                }} pathOptions={{ color: "purple" }} positions={TESTREGION} />
             </MapContainer>
         </div>;
     }
