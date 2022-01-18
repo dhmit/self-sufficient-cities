c579c088b8b499e5cbeff0d2423d9c21777f4d5d
kmorhun <kmorhun@gmail.com>
2022-01-18 14:36:35 -0500

    Attempts at geojson parsing

--- frontend/components/Map.js
+++ frontend/components/Map.js
@@ -1,6 +1,46 @@
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import React from "react";
 
+// import "calvinmetcalf/leaflet-ajax";
+
+// $.getJSON("usa_adm.geojson",function(data){
+//     // L.geoJson function is used to parse geojson file and load on to map
+//     L.geoJson(data).addTo(newMap);
+// });
+// const DCTractsGeoJson = new L.GeoJSON.AJAX("tl_2021_11_tract.geojson");
+// const DCTractsGeoJson = L.geoJson("tl_2021_11_tract.geojson");
+// console.log(L.geoJson("tl_2021_11_tract.geojson"));
+
+
+// $.ajax({
+//     type: "POST",
+//     url: "tl_2021_11_tract.geojson",
+//     dataType: 'json',
+//     success: function (response) {
+//         let DCTractsGeoJson = L.geoJson(response);
+//         console.log(DCTractsGeoJson);
+//     }
+// });
+
+// fetch("frontend/components/tl_2021_11_tract.geojson", {
+//     method: 'get'
+// }).then(function(response){
+//     // success
+//     console.log(response);
+//     });
+
+// Using Axios?????????
+
+// let xhr = new XMLHttpRequest();
+// xhr.open('GET', 'tl_2021_11_tract.geojson');
+// xhr.setRequestHeader('Content-Type', 'application/json');
+// xhr.responseType = 'json';
+// xhr.onload = function() {
+//     if (xhr.status !== 200) return;
+//     console.log(xhr.response);
+// };
+// xhr.send();
+
 export default class Map extends React.Component {
     state = {
         position: [38.9051606, -77.0036513],
