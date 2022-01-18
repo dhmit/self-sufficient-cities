0eb10878eb2873c18d83eae102e93922cf9b920a
sharafrashid2 <sharafr2@mit.edu>
2022-01-18 16:35:35 -0500

    Added code for the slider

--- frontend/components/Slider.js
+++ frontend/components/Slider.js
@@ -1,3 +1,36 @@
 import React from "react";
-import Slider, {[0, 100]} from 'rc-slider';
+import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
+
+const style = {width: 800, margin: 50};
+const marks = {};
+const minY = 1910;
+const maxY = 1920;
+
+for (let i = minY; i < maxY + 1; i++) {
+    const yearStr = String(i);
+    if (i === 1910 || i === 1920) {
+        marks[i] = {label: <strong>{yearStr}</strong>}
+    }
+    else {
+        marks[i] = yearStr
+    }
+}
+
+function log(value) {
+  console.log(value);
+}
+
+export default class Slider extends React.component {
+    render() {
+        return(
+            <div>
+                <div style = {style}>
+                    <p> Timeline Slider </p>
+                    <Slider.Range min={minY} max={maxY} marks={marks} step={10} onChange={log} defaultValue={[1900,2000]} />
+                </div>
+            </div>
+        );
+    }
+}
+
