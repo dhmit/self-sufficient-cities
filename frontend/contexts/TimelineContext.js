3b1ce8d5215211099a3688f3e8a752e71299b67e
angcast <angcast@mit.edu>
2022-01-18 15:47:53 -0500

    added interval object in context

--- frontend/contexts/TimelineContext.js
+++ frontend/contexts/TimelineContext.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
+// object that represents an interval in the timeline with a start year `start` and end year `end`
+export class Interval {
+  constructor(startYear, endYear) {
+    this.start = startYear;
+    this.end = endYear;
+  }
+}
+
 export const TimelineContext = React.createContext({
-    'startYear': 0,
-    'endYear': 0,
+    'interval': new Interval(0, 0),
 });
