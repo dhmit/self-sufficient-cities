451722279220d686fe4841342408c8c2f0407271
angcast <angcast@mit.edu>
2022-01-18 16:44:26 -0500

    added back example

--- frontend/components/global/Nav.js
+++ frontend/components/global/Nav.js
@@ -13,6 +13,9 @@ const Nav = () => {
                 <li className="nav-item mr-2">
                     <a href={"/timeline"}>Timeline</a>
                 </li>
+                <li className="nav-item mr-2">
+                    <a href={"/example/1"}>Example</a>
+                </li>
             </ul>
             <a className="lab-link" href="https://digitalhumanities.mit.edu/" target="_blank"
                 rel="noreferrer"><img className="lab-image" src={DH_LOGO}/>
