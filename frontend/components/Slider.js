1de1387ea8146f6bdc53d97eed7c6360fdecc537
sharafrashid2 <sharafr2@mit.edu>
2022-01-18 16:56:11 -0500

    Fixed the bugs in the slider component

--- frontend/components/Slider.js
+++ frontend/components/Slider.js
@@ -8,35 +8,6 @@ const minY = 1910;
 const maxY = 1920;
 
 for (let i = minY; i < maxY + 1; i++) {
-    const yearStr = String(i);
-    if (i === 1910 || i === 1920) {
-        marks[i] = {label: <strong>{yearStr}</strong>}
-    }
-    else {
-        marks[i] = yearStr
-    }
-}
-
-function log(value) {
-  console.log(value);
-}
-
-export default class Slider extends React.component {
-    render() {
-        return(
-            <div>
-                <div style = {style}>
-                    <p> Timeline Slider </p>
-                    <Slider.Range min={minY} max={maxY} marks={marks} step={10} onChange={log} defaultValue={[1900,2000]} />
-                </div>
-=======
-const style = { width: 400, margin: 50 };
-
-const marks = {}
-const minY = 1910
-const maxY = 1920
-
-for (let i = minY; i < maxY + 1; i++) {
     const str = String(i)
     if (i === minY || i === maxY) {
         marks[i] = <strong>{str}</strong>
@@ -45,17 +16,24 @@ for (let i = minY; i < maxY + 1; i++) {
 }
 
 function log(value) {
-  console.log(value)
+  console.log(value);
 }
 
-
-export default class Slider extends React.Component {
-    render(){
-        return(
-            <div style = {style}>
-                <p> Timeline Slider</p>
-                <Slider.range min={minY} marks={marks} step={10} onChange={log} defaultValue = {[20, 40]} />
+export default class Slider extends React.component {
+    render() {
+        return (
+            <div>
+                <div style={style}>
+                    <p> Timeline Slider </p>
+                    <Slider.Range min={minY} max={maxY} marks={marks} step={10} onChange={log}
+                                  defaultValue={[1900, 2000]}/>
+                </div>
             </div>
         );
+    }
 }
+
+
+
+
 
