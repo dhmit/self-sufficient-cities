292ee6d1b50a9cda1560a1c83fe6aa05d66ab48c
Quincy Johnson <35878908+qujohn314@users.noreply.github.com>
2022-01-18 11:20:32 -0500

    First iteration of the self sustaining cities db

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,25 @@
 """
 Models for the Self-Sustaining Cities web app.
 """
+
+from django.db import models
+
+
+class Location(models.Model):
+    name = models.charField(max_length=128)
+    coordinates = models.Polygon()
+
+
+class Person(models.Model):
+    first_name = models.charField(max_length=32)
+    last_name = models.charField(max_length=32)
+    ethnicity = models.charField(max_length=32, blank=True)
+    date_of_birth = models.DateField()
+    country_of_origin = models.charField(max_length=64)
+
+
+class Event(models.Model):
+    name = models.charField(max_length=128)
+    date = models.DateField()
+    locations = models.ManyToMany(Location)
+    people = models.ManyToMany(Person)
