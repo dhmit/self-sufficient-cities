d971dca440df7f1c5f40d614e8bcebf922b27904
angcast <angcast@mit.edu>
2022-01-18 17:03:10 -0500

    fixing error found on timeline

--- frontend/components/Timeline.js
+++ frontend/components/Timeline.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import {Interval, TimelineContext} from "../contexts/TimelineContext";
 
-export function Timeline()  {
+export function Timeline() {
     const [interval, setInterval] = useState(new Interval(0, 0));
     const contextState = {
         interval,
