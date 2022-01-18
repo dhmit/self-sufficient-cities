6992e7fe14c8fae7765e34ad1627a170087f7978
sharafrashid2 <sharafr2@mit.edu>
2022-01-18 16:41:49 -0500

    Added code to slider

--- frontend/components/Slider.js
+++ frontend/components/Slider.js
@@ -2,6 +2,7 @@ import React from "react";
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 
+<<<<<<< Updated upstream
 const style = {width: 800, margin: 50};
 const marks = {};
 const minY = 1910;
@@ -29,8 +30,38 @@ export default class Slider extends React.component {
                     <p> Timeline Slider </p>
                     <Slider.Range min={minY} max={maxY} marks={marks} step={10} onChange={log} defaultValue={[1900,2000]} />
                 </div>
+=======
+const style = { width: 400, margin: 50 };
+
+const marks = {}
+const minY = 1910
+const maxY = 1920
+
+for (let i = minY; i < maxY + 1; i++) {
+    const str = String(i)
+    if (i === minY || i === maxY) {
+        marks[i] = <strong>{str}</strong>
+    }
+    marks[i] = str
+}
+
+function log(value) {
+  console.log(value); //eslint-disable-line
+}
+
+
+export default class Slider extends React.Component {
+    render() {
+        return(
+            <div style = {style}>
+                <p> Timeline Slider</p>
+                <Slider.range min={minY} marks={marks} step={10} onChange={log} defaultValue = {[20, 40]} />
+>>>>>>> Stashed changes
             </div>
         );
     }
 }
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
