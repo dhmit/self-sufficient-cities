6d81e67a96c5d0bfa8468c21f1d8179dedf47ca8
Raxel Gutierrez <raxelgutierrez09@gmail.com>
2022-01-19 16:28:26 -0500

    Add wrap around for documents in showcase

--- frontend/components/ShowcaseItem.js
+++ frontend/components/ShowcaseItem.js
@@ -5,7 +5,7 @@ import {Card} from "react-bootstrap";
 const ShowcaseItem = ({title, date, imageRef}) => {
 
     return (
-        <Card className="showcase-item" style={{ width: '20rem' }}>
+        <Card className="showcase-item">
           <Card.Body>
             <Card.Title> { title }</Card.Title>
             <Card.Subtitle className="mb-2 text-muted"> { date } </Card.Subtitle>
