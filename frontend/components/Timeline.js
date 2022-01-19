3ef95648c9004d3c3edb9643ae8b11c999715f9e
sharafrashid2 <sharafr2@mit.edu>
2022-01-19 15:29:40 -0500

    Temporarily removed slider from page to fix a bug

--- frontend/components/Timeline.js
+++ frontend/components/Timeline.js
@@ -1,7 +1,6 @@
 import React, {useState} from "react";
 import {Interval, TimelineContext} from "../contexts/TimelineContext";
 import DocumentShowcase from "./DocumentShowcase";
-import Slider from "./Slider";
 
 
 export function Timeline() {
@@ -15,7 +14,6 @@ export function Timeline() {
         <React.Fragment>
             <TimelineContext.Provider value={contextState}>
                 <p> This is our timeline! </p>
-                <Slider/>
                 <DocumentShowcase/>
             </TimelineContext.Provider>
         </React.Fragment>
