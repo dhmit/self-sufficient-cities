552fb813f3da8d4b7d1846b8d423b4923192ef07
sharafrashid2 <sharafr2@mit.edu>
2022-01-21 15:50:57 -0500

    slider tests

--- frontend/components/Slider.js
+++ frontend/components/Slider.js
@@ -4,12 +4,15 @@ import {Interval, TimelineContext} from "../contexts/TimelineContext";
 import * as PropTypes from "prop-types";
 import 'rc-slider/assets/index.css';
 
-const style = {width: 1375, margin: 0};
+const style = {width: 1375, margin: 0} ;
 const marks = {};
+
+// Can change min and max years of sliders here
 const minY = 1910;
 const maxY = 2000;
 
 
+// Creates markings for the slider
 for (let i = minY; i < maxY + 1; i += 5) {
     const str = String(i);
     if (i === minY || i === maxY) {
@@ -33,19 +36,23 @@ function log(value) {
 
 const TimeSlider = ({leftEnd, rightEnd}) => {
         const state = useContext(TimelineContext);
-        /*function change() {
-            const interval = new Interval(leftVal, rightVal);
-            state.setTimelineRange(interval);
-        }*/
+
         return (
             <div id = "slider">
                 <div style={style}>
-                    <Slider.Range min={minY} max={maxY} dots={true} marks={marks} step={1} onChange={log}
+                    <Slider.Range min={minY} max={maxY} dots={true} marks={marks} step={5} onChange={log}
                                   value = {[state.intervalSelected.start, state.intervalSelected.end]} />
-                <div id = "line-break">
+               {/* <div id = "line-break">
                     <br/>
+                </div>*/}
                 </div>
-                </div>
+
+                {/*<div id = "overlay">
+                    <div style = {style}>
+                        <Slider.Range min={minY} max={maxY} dots={true} marks={marks} step={1} onChange={log}
+                                  defaultValue = {[state.intervalSelected.start, state.intervalSelected.end]} />
+                    </div>
+                </div>*/}
             </div>
         );
 };
