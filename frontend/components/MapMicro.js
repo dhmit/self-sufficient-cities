079a4efd17c9712e3135281ae24f35f6482ed41b
anastasia <anastasia.aizman@gmail.com>
2022-01-25 16:12:58 -0500

    fix - syntax errors, lint errors

--- frontend/components/MapMicro.js
+++ frontend/components/MapMicro.js
@@ -4,105 +4,105 @@ import Input from "@material-ui/core/Input";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Slider from "@material-ui/core/Slider";
-import Dropdown from 'react-bootstrap/Dropdown';
+import Dropdown from "react-bootstrap/Dropdown";
 // AFTERNOON TEAM
 
 const ADDRESS_DATA = [
     {
-        name: 'For rent: 2124 8th St',
-        coordinates: [38.9183777,-77.0254307],
-        year: 1915,
+        name: "or rent: 2124 8th St",
+        coordinates: [38.9183777, -77.0254307],
+        year: 1915
     },
     {
-        name: 'For rent: 2124 11th St.',
-        coordinates: [38.9187446,-77.0296913],
-        year: 1915,
+        name: "or rent: 2124 11th St.",
+        coordinates: [38.9187446, -77.0296913],
+        year: 1915
     },
     {
-        name: 'For rent: 1139 6th St.',
-        coordinates: [38.9053336,-77.0217962],
-        year: 1915,
+        name: "or rent: 1139 6th St.",
+        coordinates: [38.9053336, -77.0217962],
+        year: 1915
     },
     {
-        name: 'For rent: 3108 Sherman Ave. N. W.',
-        coordinates: [38.9290822,-77.0287721],
-        year: 1913,
+        name: "or rent: 3108 Sherman Ave. N. W.",
+        coordinates: [38.9290822, -77.0287721],
+        year: 1913
     },
     {
-        name: 'For rent: 1005 Maryland Ave. S. W.',
-        coordinates: [38.8844613,-77.0303063],
-        year: 1913,
+        name: "or rent: 1005 Maryland Ave. S. W.",
+        coordinates: [38.8844613, -77.0303063],
+        year: 1913
     },
     {
-        name: 'For rent: 41 Patterson St. N. E.',
-        coordinates: [38.9062153,-77.0099283],
-        year: 1913,
+        name: "or rent: 41 Patterson St. N. E.",
+        coordinates: [38.9062153, -77.0099283],
+        year: 1913
     },
     {
-        name: 'For rent: 1045 47th St.',
-        coordinates: [38.9045554,-76.9366048],
-        year: 1913,
+        name: "or rent: 1045 47th St.",
+        coordinates: [38.9045554, -76.9366048],
+        year: 1913
     },
     {
-        name: 'For rent: 2047 9th St. N. W.',
-        coordinates: [38.9181871,-77.02582],
-        year: 1913,
+        name: "or rent: 2047 9th St. N. W.",
+        coordinates: [38.9181871, -77.02582],
+        year: 1913
     },
     {
-        name: 'For rent: 2654 15th St. N. W.',
-        coordinates: [38.9245707,-77.0378564],
-        year: 1913,
+        name: "or rent: 2654 15th St. N. W.",
+        coordinates: [38.9245707, -77.0378564],
+        year: 1913
     },
     {
-        name: 'For rent: 506 Fifth Street Northwest',
-        coordinates: [38.8967959,-77.0215881],
-        year: 1913,
+        name: "or rent: 506 Fifth Street Northwest",
+        coordinates: [38.8967959, -77.0215881],
+        year: 1913
     },
     {
-        name: 'Pianoforte Lessons, Mrs. M. Harvey Clickscales: 1232 Linden Street Northeast',
-        coordinates: [38.8997081,-76.991249],
-        year: 1913,
+        name: "ianoforte Lessons, Mrs. M. Harvey Clickscales: 1232 Linden Street Northeast",
+        coordinates: [38.8997081, -76.991249],
+        year: 1913
     },
     {
-        name: 'Land for Sale: Miss Elizabeth Shaw, 1613 Thirteenth Street Northwest',
-        coordinates: [38.8688428,-76.9892379],
-        year: 1913,
+        name: "and for Sale: Miss Elizabeth Shaw, 1613 Thirteenth Street Northwest",
+        coordinates: [38.8688428, -76.9892379],
+        year: 1913
     },
     {
-        name: 'The Washington Bee: 1109 Eye Street Northwest',
-        coordinates: [38.9016879,-77.0293254],
-        year: 1913,
+        name: "he Washington Bee: 1109 Eye Street Northwest",
+        coordinates: [38.9016879, -77.0293254],
+        year: 1913
     },
     {
-        name: 'Dr. Geo. H. Richardson: 309 Eleventh St. N. E.',
-        coordinates: [38.8938675,-76.993485],
-        year: 1913,
+        name: "r. Geo. H. Richardson: 309 Eleventh St. N. E.",
+        coordinates: [38.8938675, -76.993485],
+        year: 1913
     },
     {
-        name: 'A.D. Powell, Dealer in Coal, Wood, and Ice: 1200 R Street N. W.',
-        coordinates: [38.9126088,-77.0301461],
-        year: 1913,
+        name: ".D. Powell, Dealer in Coal, Wood, and Ice: 1200 R Street N. W.",
+        coordinates: [38.9126088, -77.0301461],
+        year: 1913
     },
     {
-        name: 'Xanders Famous Cocktails: 909 7th St.',
-        coordinates: [38.901508,-77.0236903],
-        year: 1913,
+        name: "anders Famous Cocktails: 909 7th St.",
+        coordinates: [38.901508, -77.0236903],
+        year: 1913
     },
     {
-        name: 'Elite Photo Studio: 1814 Fourteenth Street, N. W.',
-        coordinates: [38.9146532,-77.0344107],
-        year: 1913,
+        name: "lite Photo Studio: 1814 Fourteenth Street, N. W.",
+        coordinates: [38.9146532, -77.0344107],
+        year: 1913
     },
     {
-        name: 'For Rent, High Class Apartments; The Minerva: 1838 Fourth Street Northwest',
-        coordinates: [38.9154674,-77.0197966],
-        year: 1913,
+        name: "or Rent, High Class Apartments; The Minerva: 1838 Fourth Street Northwest",
+        coordinates: [38.9154674, -77.0197966],
+        year: 1913
     },
     {
-        name: 'Shannon & Luchs, Renting Service: 713 Fourteenth St. N. W.',
-        coordinates: [38.89867,-77.0337179],
-        year: 1913,
-    },
+        name: "hannon & Luchs, Renting Service: 713 Fourteenth St. N. W.",
+        coordinates: [38.89867, -77.0337179],
+        year: 1913
+    }
 ];
 
 const MAIN_LOCATION = {
@@ -135,7 +135,8 @@ function sliderInput(value, bound, defaultRange, inputChangeFunc, sliderBlurFunc
 
 
 function timeSlider(
-    sliderName, currentRange, defaultRange, lastValid, sliderChangeFunc, inputChangeFunc, sliderBlurFunc
+    sliderName, currentRange, defaultRange, lastValid,
+    sliderChangeFunc, inputChangeFunc, sliderBlurFunc
 ) {
     const [minValue, maxValue] = defaultRange;
     return (
@@ -194,8 +195,8 @@ export default class MapMicro extends React.Component {
 
     setSliderValue(newLowerBound, newUpperBound) {
         this.setState({
-            sliderState:[newLowerBound, newUpperBound],
-            lastValid: [newLowerBound, newUpperBound],
+            sliderState: [newLowerBound, newUpperBound],
+            lastValid: [newLowerBound, newUpperBound]
         });
     }
 
@@ -231,13 +232,13 @@ export default class MapMicro extends React.Component {
 
         this.setState({
             sliderState: newSliderState,
-            lastValid: newValidState,
+            lastValid: newValidState
         });
     };
 
     handleSliderBlur = () => {
         // Used when slider changed by dragging after changing inputs
-        // Needed if inputs are not bounded by the slider's maximum and minimum values
+        // Needed if inputs are not bounded by the slider" maximum and minimum values
         const [currentLowerValue, currentUpperValue] = this.state.sliderState;
         const [minValue, maxValue] = this.state.timeRange;
         if (currentLowerValue < minValue) {
@@ -249,7 +250,7 @@ export default class MapMicro extends React.Component {
 
     render() {
         const currentMarkerObjects = this.state.markerData.filter((location) => (
-            location.year>=this.state.lastValid[0] && location.year<=this.state.lastValid[1])
+            location.year >= this.state.lastValid[0] && location.year <= this.state.lastValid[1])
         );
 
         const markerObjects = currentMarkerObjects.map((location, i) => (
@@ -280,10 +281,11 @@ export default class MapMicro extends React.Component {
                 </div>
                 <div id="map">
                     <MapContainer
-                        center={this.state.mainLocation.coordinates} zoom={13} scrollWheelZoom={true}
+                        center={this.state.mainLocation.coordinates} zoom={13}
+                        scrollWheelZoom={true}
                     >
                         <TileLayer
-                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
                         {markerObjects}
@@ -295,7 +297,7 @@ export default class MapMicro extends React.Component {
                         this.state.lastValid,
                         this.handleSliderChange,
                         this.handleSliderInputChange,
-                        this.handleSliderBlur,
+                        this.handleSliderBlur
                     )}
                 </div>
             </div>
