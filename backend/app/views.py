d28fbe03fe77fd16ffe241385ffcdd63fbc0daf2
MasterEchez <32005788+MasterEchez@users.noreply.github.com>
2022-01-25 22:58:55 -0500

    remove unused imports

--- backend/app/views.py
+++ backend/app/views.py
@@ -24,8 +24,7 @@ context = {
 }
 """
 import json
-import datetime
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
