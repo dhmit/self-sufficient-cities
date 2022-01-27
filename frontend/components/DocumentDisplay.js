6dc131c51187849d562e772f3cb103a8dbd2934f
Nisha-Nathan <nishabk@mit.edu>
2022-01-27 17:55:55 -0500

    Updated magnifying tool
    Enabled users to enter their magnification scale and use it

--- frontend/components/DocumentDisplay.js
+++ frontend/components/DocumentDisplay.js
@@ -3,6 +3,7 @@ import * as PropTypes from "prop-types";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TEST_IMAGE from "../images/testing.jpg";
 import Magnifier from "react-magnifier";
+import {number} from "prop-types";
 
 /**
  * Displays the Document and provides an option to zoom in
@@ -10,25 +11,28 @@ import Magnifier from "react-magnifier";
  *
  */
 const DocumentDisplay = ({document}) => {
-    const [Scale, setScale] = useState(1.5);
-    const handleIncrement = () =>
-        setScale(currentCount => currentCount + 0.1);
-    const handleDecrement = () =>
-        setScale(currentCount => currentCount - 0.1);
+    const [Scale, setScale] = useState(Scale);
+
 
 
     // TODO: call magnifying feature with document image from backend
     return(
         <>
+
             <Magnifier className="doc-image" src={document.image ? document.image : TEST_IMAGE}
                 mgShape='square' mgHeight={300} mgWidth={300}
                 mgMouseOffsetX={-50} mgMouseOffsetY={50} zoomFactor={Scale}/>
-            <h5>Magnification Scale: {Scale.toFixed(2)}</h5>
-            <button className="btn btn-outline-secondary mx-2" type="button"
-                onClick={handleIncrement}>Increase</button>
-            <button className="btn btn-outline-secondary" type="button"
-                onClick={handleDecrement}>Decrease </button>
 
+            <label className="labels">Magnification Scale:   </label>
+            <input
+                     type="number"
+                     min="1.0"
+                     max="5.0"
+                     value={Scale}
+                     placeholder="1.0"
+                     onChange={e => setScale(e.target.value)}
+            />
+            
         </>
     );
 };
