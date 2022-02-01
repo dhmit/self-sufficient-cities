4e0f19c461baf98bc0702b2bf1b82bd4f794f616
anastasia <anastasia.aizman@gmail.com>
2022-02-01 13:55:56 -0500

    fix - variable name change, removing commented out lines

--- frontend/components/MapMacro.js
+++ frontend/components/MapMacro.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React from "react";
-import {MapContainer, Marker, Popup, TileLayer, GeoJSON} from "react-leaflet";
+import {MapContainer, Marker, TileLayer, GeoJSON} from "react-leaflet";
 import Table from "./Table";
 
 // MORNING TEAM
@@ -31,19 +31,17 @@ export default class MapMacro extends React.Component {
     }
 
     showTableData(event) {
-        // console.log(event.latlng.lat + " " + event.latlng.lng);
         event.target.setStyle({
             fillColor: "green"
         });
     }
 
     onEachBlock = (block, layer) => {
+        console.log("onEachBlock");
         const blockName = block.properties.NAMELSAD;
-        // layer.bindPopup(blockName);
 
         layer.on({
             click: this.showTableData,
-
             mouseover: (event) => {
                 event.target.bindPopup(blockName).openPopup();
             }
@@ -54,17 +52,13 @@ export default class MapMacro extends React.Component {
         return <div id="map">
             <h1>{this.state.location}</h1>
             <MapContainer center={this.state.position} zoom={13} scrollWheelZoom={true}>
-                <Marker position={this.state.position}>
-                    <Popup>
-                        A pretty CSS3 popup. <br/> Easily customizable.
-                    </Popup>
-                </Marker>
+                <Marker position={this.state.position}/>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                {Object.keys(this.state.censusdata).length > 0 &&
-                    <GeoJSON data={this.state.censusdata} onEachFeature={this.onEachBlock}/>
+                {Object.keys(this.state.censustract).length > 0 &&
+                    <GeoJSON data={this.state.censustract} onEachFeature={this.onEachBlock}/>
                 };
             </MapContainer>
 
