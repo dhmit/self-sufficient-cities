87f39af40152f9b7ebc91dca404207a8846aa993
anastasia <anastasia.aizman@gmail.com>
2022-02-08 13:40:34 -0500

    update - remove unnecessary really heavy swagger UI

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -72,12 +72,7 @@ schema_view = get_schema_view(
 )
 
 swagger_urls = [
-    re_path(r'^api(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0),
-            name='schema-json'),
-    re_path(r'^api/$', schema_view.with_ui('swagger', cache_timeout=0),
-            name='schema-swagger-ui'),
-    re_path(r'^docs/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
-
+    re_path(r'^api/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
 ]
 
 # Adding it all together
