363821048cc394c7d6d07a7bebf60238e713572f
pauladc <pauladc@mit.edu>
2022-02-20 19:07:49 -0500

    added donut chart

---
+++ frontend/components/DonutChart.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const data = {
+  labels: ['Native-Born White', 'African American', 'Foreign Born White', 'Other Races'],
+  datasets: [
+    {
+      label: 'Population by Race and Nativity in Deanwood, 1940',
+      data: [2412, 9777, 186, 4],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+export function App() {
+  return <Doughnut data={data} />;
+}
+
