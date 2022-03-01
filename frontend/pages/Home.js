3d401e924344826f341f85f9274a161d1fa85d1c
Samantha York <samimak@mit.edu>
2022-03-01 12:50:46 -0500

    Created framework to display cities from data
    Created dummy data that will serve as the framework for displaying city cards on the homepage, and removed the direct hard coding of cities

--- frontend/pages/Home.js
+++ frontend/pages/Home.js
@@ -4,6 +4,33 @@ import {Container, Row, Col} from "react-bootstrap";
 import HomePageCard from "../components/home/HomePageCard";
 import dh_logo from "../images/dh_logo.svg";
 
+const dummy_city_data = [
+    {
+        img: dh_logo,
+        title: "DEANWOOD, D.C.",
+        text: "Explore the rise and fall of a self-sustaining neighborhood right in the US" +
+            " capital.",
+        resources: ["oral_history", "map", "timeline"]
+    },
+    {
+        img: dh_logo,
+        title: "DETROIT, MI",
+        text: "Filler Text!",
+        resources: ["map", "oral_history"]
+    },
+    {
+        img: dh_logo,
+        title: "MEMPHIS, TN",
+        text: "Filler Text!",
+        resources: ["map", "timeline", "oral_history"]
+    },
+    {
+        img: dh_logo,
+        title: "Sampleville",
+        text: "More coming soon?",
+        resources: ["oral_history", "timeline"]
+    }
+];
 
 export default class Home extends React.Component {
     render() {
@@ -22,31 +49,17 @@ export default class Home extends React.Component {
                     </Col>
                     <Col>
                         <Row xs={1} md={2}>
-                            <HomePageCard
-                                img_source={dh_logo}
-                                title='DEANWOOD, D.C.'
-                                text='Explore the rise and fall of a self-sustaining neighborhood
-                                right in the US capital.'
-                                resources={["oral_history", "map", "timeline"]}
-                            />
-                            <HomePageCard
-                                img_source={dh_logo}
-                                title='DETROIT, MI'
-                                text='Filler text!'
-                                resources={["map", "oral_history"]}
-                            />
-                            <HomePageCard
-                                img_source={dh_logo}
-                                title='MEMPHIS, TN'
-                                text='Filler text!'
-                                resources={["map", "timeline", "oral_history"]}
-                            />
-                            <HomePageCard
-                                img_source={dh_logo}
-                                title='Sampleville'
-                                text='More text coming soon?'
-                                resources={["oral_history", "timeline"]}
-                            />
+                            {dummy_city_data.map((data, idx) => {
+                                return (
+                                    <HomePageCard
+                                        img_source={data.img}
+                                        title={data.title}
+                                        text={data.text}
+                                        resources={data.resources}
+                                        key={idx}
+                                    />
+                                );
+                            })}
                         </Row>
                     </Col>
                 </Row>
