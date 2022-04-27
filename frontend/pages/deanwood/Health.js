669387e29d9383426edac8b37ba16673a3ee1c1d
Guilhermo Cutrim Costa <guicosta@mit.edu>
2022-04-27 13:32:58 -0400

    Merge health pages

--- frontend/pages/deanwood/Health.js
+++ frontend/pages/deanwood/Health.js
@@ -1,29 +1,42 @@
-import React from "react";
-import {Container, Row, Col} from "react-bootstrap";
+import React, {useState} from "react";
+import {Container, Row, Col, Button} from "react-bootstrap";
 import * as PropTypes from "prop-types";
-import DeanwoodNav from "./DeanwoodNav";
-// import Lifeexpectancy from "./Lifeexpectancy";
+import DeanwoodCovid from "./Covid";
+import DeanwoodResident from "./Resident";
 import HealthTrends from "./Health_Trends";
+
 
 // if putting resource bar back in, add {resources} within parameter parentheses below
 export const DeanwoodHealth = ({resources}) => {
-
+    const tabs = [
+        {label: "covidData", name: "COVID-19 Data", el: <DeanwoodCovid resources={resources}/>},
+        {label: "healthTrends", name: "Health Trends", el: <HealthTrends resources={resources}/>},
+        {label: "residentProfile", name: "Resident Profile", el: <DeanwoodResident resources={resources}/>}
+    ];
+    const [displayState, setDisplayState] = useState("healthTrends");
     return (<>
         <Container className="city" id="deanwood-overview">
             <Row>
                 <h1>Health</h1>
                 <h3>COVID in Deanwood: Ward 3 vs. Ward 7</h3>
-                <DeanwoodNav selected={"covid_data"} resources={resources}/>
-            </Row>
-            <Row>
-                <HealthTrends/>
-
             </Row>
 
             <Row>
-                <Col>
-                    <p>Home page with any necessary intro information!</p>
-                </Col>
+                {tabs.map((tab, idx) =>
+                    <Col key={idx}>
+                        <Button key={idx} onClick={() => setDisplayState(tab.label)}>
+                            {tab.name}
+                        </Button>
+                    </Col>
+                )}
+            </Row>
+
+            <Row>
+                {tabs.map((tab, idx) =>
+                    <Container key={idx}>
+                        {displayState === tab.label && tab.el}
+                    </Container>
+                )}
             </Row>
         </Container>
 
