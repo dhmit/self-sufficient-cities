201d0cd803aea444a687c62ec5208a23157ba004
Quincy Johnson <35878908+qujohn314@users.noreply.github.com>
2022-04-29 16:26:07 -0400

    Basic Page Layout

--- frontend/pages/deanwood/DeanwoodFood.js
+++ frontend/pages/deanwood/DeanwoodFood.js
@@ -7,89 +7,30 @@ import MapDeanwood from "../../components/maps/MapDeanwood";
 export const DeanwoodFood = ({resources}) => {
 
     return (<>
-        <Container className="city" id="deanwood-overview">
+        <Container className="food" id="deanwood-food">
             <Row >
-                <Col md={3} className="nav-col mr-2">
-                    <h1>Deanwood, D.C.</h1>
-                    <p>
-                        The project tells the rise and fall of urban communities that grew their
-                        own food in the 20th century United States. Taking the Deanwood
-                        neighborhood in Washington, D.C. as their starting place, students
-                        consulted newspaper articles and census data to design an interactive
-                        site.
-                    </p>
-                    <DeanwoodNav selected={"overview"} resources={resources}/>
-                </Col>
-                <Row xs={1} md={2} className="justify-content-around mt-5">
-                    <Col md={4}/>
-                    <Col md={4}>
-                        <p className={"intro-text"}>
-                            Eligendi excepturi corporis velit provident dicta neque autem. Enim ab
-                            at
-                            distinctio enim debitis temporibus. Provident enim natus cumque.
-                            Quibusdam
-                            impedit nam et ipsam. Consequatur earum quam dolore doloremque earum.…
-                            Eligendi excepturi corporis velit provident dicta neque autem.
-                            Enim ab at distinctio enim debitis temporibus.
-                            Provident enim natus cumque. Quibusdam impedit nam
-                            et ipsam. Consequatur earum quam dolore doloremque earum.…
-                        </p>
-                    </Col>
-                    <Col md={4}>
-                        <blockquote>
-                            "This is a blockquote. Eligendi excepturi corporis velit. Enim
-                            ab at distinctio enim debitis temporibus"
-                        </blockquote>
-                    </Col>
-                </Row>
+                {/*MENU BAR*/}
+                <Row><h1>Menu Bar</h1></Row>
+                {/*HOW TO USE BOX*/}
                 <Row>
-                    <Col md={4}/>
-                    <Col><MapDeanwood/></Col>
+                    <h1>
+                        Help Box
+                    </h1>
                 </Row>
-                <Row className="mt-3">
-                    <Col md={4}/>
-                    <Col md={1}/>
-                    <Col md={6}>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-                            nonummy
-                            nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut
-                            wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper
-                            suscipit
-                            lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum
-                            iriure
-                            dolor in hendrerit in vulputate velit esse molestie consequat, vel illum
-                            dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio
-                            dignissim qui blandit praesent luptatum zzril delenit augue duis dolore
-                            te
-                            feugait nulla facilisi.
-                            Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam
-                            nonummy
-                            nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut
-                            wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper
-                            suscipit
-                            lobortis nisl ut aliquip ex ea commodo consequat.
-                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-                            nonummy
-                            nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut
-                            wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper
-                            suscipit
-                            lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum
-                            iriure
-                            dolor in hendrerit in vulputate velit esse molestie consequat, vel illum
-                            dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio
-                            dignissim qui blandit praesent luptatum zzril delenit augue duis dolore
-                            te
-                            feugait nulla facilisi.
-                            Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam
-                            nonummy
-                            nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut
-                            wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper
-                            suscipit
-                        </p>
+                {/*MAP AND INFO BOX*/}
+                <Row lg = {2} style={{backgroundColor:"green"}}>
+                    <Col style={{backgroundColor:"red"}}>
+                        <h1>
+                            MAP
+                        </h1>
                     </Col>
-                    <Col md={1}/>
+                    <Col style={{backgroundColor:"blue"}}>
+                        <h1>
+                            LOCATION INFO
+                        </h1>
+                    </Col>
                 </Row>
+
             </Row>
         </Container>
     </>);
