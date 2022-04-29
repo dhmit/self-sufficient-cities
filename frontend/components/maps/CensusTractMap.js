a0f80488aea7c06f128189f0dd6c23ed005d349c
Samantha York <samimak@mit.edu>
2022-04-29 14:39:50 -0400

    Create Popups for Census Tracts
    Created popups for the individual census tracts in `CensusTractMap`

--- frontend/components/maps/CensusTractMap.js
+++ frontend/components/maps/CensusTractMap.js
@@ -1,4 +1,5 @@
 import React from "react";
+import * as ReactDomServer from "react-dom/server";
 import * as PropTypes from "prop-types";
 
 import {MapContainer, TileLayer, GeoJSON} from "react-leaflet";
@@ -13,12 +14,61 @@ class CensusTractMap extends React.Component {
             closest_distance: 0,
             farthest_distance: 0
         };
+        this.styleTracts = this.styleTracts.bind(this);
+        this.prep_distance_data = this.prep_distance_data.bind(this);
+        this.onEachFeature = this.onEachFeature.bind(this);
+        this.generatePopupContent = this.generatePopupContent.bind(this);
     }
 
     componentDidUpdate(prevProps, _prevState, _ss) {
         if (prevProps !== this.props) this.prep_distance_data();
     }
 
+    /**
+     * A method that generates the content for the popup that belongs to a census tract
+     *
+     * @param name - the full name of the census tract to be displayed
+     * @param distance the similarity score for the given tract
+     */
+    generatePopupContent(name, distance) {
+        return (
+            <div className={"tract-popup-content"}>
+                <b>{name}</b>
+                <div>Similarity Score: {distance}</div>
+                <em>Lower is better!</em>
+            </div>
+        );
+    }
+
+    /**
+     * A method used for giving each Census Tract its own pop up that activates on click.
+     *
+     * The popup contains information containing its closeness to Deanwood and its full name.
+     *
+     * @param feature - the GeoJSON feature that will have the popup applied
+     * @param layer - the layer that the GeoJSON feature
+     */
+    onEachFeature(feature, layer) {
+
+        const tract_code = feature.properties["GISJOIN"];
+
+        let tract_name;
+        let tract_distance;
+        if (tract_code === DEANWOOD_JOIN_CODE) {
+            tract_name = "Census Tract 78 (Deanwood), District of Columbia";
+            tract_distance = 0;
+        } else if (!(tract_code in this.props.deanwood_similarities)) {
+            return;
+        } else {
+            tract_name = this.props.deanwood_similarities[tract_code]["full_name"];
+            tract_distance = this.props.deanwood_similarities[tract_code]["distance"].toFixed(2);
+        }
+
+        const popupContent = ReactDomServer.renderToString(
+            this.generatePopupContent(tract_name, tract_distance)
+        );
+        layer.bindPopup(popupContent);
+    }
 
     /**
      * Generates the styling for a given census tract data point, and returns the style object
@@ -68,12 +118,19 @@ class CensusTractMap extends React.Component {
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         url="https://stamen-tiles-a.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png"
                     />
-                    {Object.keys(this.props.census_tracts).length > 0 &&
+                    {this.props.census_tracts && this.props.deanwood_similarities &&
                         this.props.census_tracts.map(tract => {
-                            return (<GeoJSON key={tract.properties["GISJOIN"]}
-                                data={tract} style={() => this.styleTracts(tract)}/>);
+                            return (
+                                <GeoJSON
+                                    key={tract.properties["GISJOIN"]}
+                                    data={tract}
+                                    style={() => this.styleTracts(tract)}
+                                    onEachFeature={this.onEachFeature}
+                                />
+                            );
                         })
-                    };
+
+                    }
                 </MapContainer>
             </div>
         );
