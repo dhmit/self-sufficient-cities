2aff43b612f88cadada134f5b9736cc2f4bd1f92
anastasia <anastasia.aizman@gmail.com>
2022-05-03 18:18:24 -0400

    remove - unused imports

--- frontend/pages/deanwood/DeanwoodFood.js
+++ frontend/pages/deanwood/DeanwoodFood.js
@@ -2,9 +2,8 @@ import React from "react";
 import {Container, Row, Col} from "react-bootstrap";
 import * as PropTypes from "prop-types";
 import DeanwoodNav from "./DeanwoodNav";
-import MapDeanwood from "../../components/maps/MapDeanwood";
+// import MapDeanwood from "../../components/maps/MapDeanwood";
 import FoodMap from "../../components/maps/FoodMap";
-import DeanwoodOverview from "./DeanwoodOverview";
 
 export const DeanwoodFood = ({resources}) => {
     return (
