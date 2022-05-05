5a1379f1e45e05265752dafcafdbd2bcf27c6e51
anastasia <anastasia.aizman@gmail.com>
2022-05-05 16:08:10 -0400

    update - health data structure. still baffled at problem of flow, so included terrible hack (added text and set visibility to zero)

--- frontend/components/health/Covid.js
+++ frontend/components/health/Covid.js
@@ -1,69 +1,56 @@
 import React from "react";
-import {Row, Col, Image, Card, Container} from "react-bootstrap";
+import {Row, Col, Image} from "react-bootstrap";
 import cases_graph from "../../images/cases_graph.png";
 import lives_graph from "../../images/cases_graph.png";
 import tests_graph from "../../images/cases_graph.png";
-import deanwood_image from "../../images/deanwood.jpg";
-const image_source = "https://commons.wikimedia.org/wiki/File:Deanwood_Washington_DC.jpg";
 
 
 export const DeanwoodCovid = () => {
 
-    return (<>
-        {/*<Container>*/}
-        {/*    <Card>*/}
-        {/*        /!*<Card.Img src={deanwood_image}/>*!/*/}
-        {/*        /!*<Card.ImgOverlay>*!/*/}
-        {/*        /!*    Licenced under CC BY 3.0. <a href={image_source}>Source.</a>*!/*/}
-        {/*        /!*</Card.ImgOverlay>*!/*/}
-        {/*        <Card.Body>*/}
-        {/*            <Card.Title>A history of disparity</Card.Title>*/}
-        {/*            <Card.Text>*/}
-        {/*                Deanwood lies in Ward 7 of the District of Columbia, home to over*/}
-        {/*                80 thousand (mostly Black) inhabitants. A couple of miles across the*/}
-        {/*                Anacostia lies Ward 3, with about the same number of (mostly White)*/}
-        {/*                inhabitants. However, the small distance between the two wards belies*/}
-        {/*                a world of difference. The disparity between Ward 3 and Ward 7 is not*/}
-        {/*                just demographic or economic: as shown here, it extends to people's*/}
-        {/*                health. Those who, by accident of birth, lie in the wrong side of the*/}
-        {/*                divide can expect to lead lives that are ten years shorter and have*/}
-        {/*                higher rates of chronic diseases such as diabetes.*/}
-        {/*            </Card.Text>*/}
-        {/*        </Card.Body>*/}
-        {/*    </Card>*/}
-        {/*</Container>*/}
-
+    return (
         <>
-            <Col className={"graph-column p-0"}>
-                <h5>Positive Cases:</h5>
-                {/*<Image fluid={true} src={cases_graph}/>*/}
-                <p>*Info about above data*</p>
-            </Col>
-            <Col className={"text-column p-0"}>
-                Hello
-            </Col>
-                {/*<Row>*/}
-                {/*    <Col className={"p-0"}>*/}
-                {/*        <h5>Lives Lost:</h5>*/}
-                {/*        /!*<Image fluid={true} src={lives_graph}/>*!/*/}
-                {/*        <p>*Info about above data*</p>*/}
-                {/*    </Col>*/}
-                {/*    <Col className={"p-0"}>Sup</Col>*/}
-                {/*</Row>*/}
-                {/*<Row>*/}
-                {/*    <Col className={"p-0"}>*/}
-                {/*        <h5>Total Tests:</h5>*/}
-                {/*        /!*<Image fluid={true} src={tests_graph}/>*!/*/}
-                {/*        <p>*Info about above data*</p>*/}
-                {/*    </Col>*/}
-                {/*    <Col className={"p-0"}>*/}
-                {/*        Here*/}
-                {/*    </Col>*/}
-                {/*</Row>*/}
+            <Row className={"p-0"}>
+                <Col md={6} className={"p-0"}>
+                    <Image fluid={true} src={cases_graph}/>
+                </Col>
+                <Col md={6} className={"p-0 health-description"}>
+                    <h5>Positive Cases:</h5>
+                    *Info about
 
-        </>
+                    <span style={{"visibility": "hidden"}}>
+                        I don't know yet why this is necessary, but the only thing' +
+                        ' that's seeming to give this div any width and put it back
+                        into the intended box model flow is words. So here they are.</span>
+                    <div className={"empty-space"}>
+                    </div>
+                </Col>
+            </Row>
 
-    </>);
+            <Row>
+                <Col className={"p-0"}>
+                    <Image fluid={true} src={lives_graph}/>
+                </Col>
+                <Col className={"p-0"}>
+                    <p>
+                        <h5>Lives Lost:</h5>
+                        *Info about above data*
+                    </p>
+
+
+                </Col>
+            </Row>
+            <Row>
+                <Col className={"p-0"}>
+                    <Image fluid={true} src={tests_graph}/>
+                </Col>
+                <Col className={"p-0"}>
+                    <p>
+                        <h5>Total Tests:</h5>
+                        *Info about above data*
+                    </p>
+                </Col>
+            </Row>
+        </>);
 
 };
 
--- frontend/pages/deanwood/Health.js
+++ frontend/pages/deanwood/Health.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import {Container, Row, Col, Button} from "react-bootstrap";
 import * as PropTypes from "prop-types";
-import DeanwoodCovid from "./Covid";
+import DeanwoodCovid from "../../components/health/Covid";
 import DeanwoodResident from "../../components/health/Resident";
 import HealthTrends from "../../components/health/Health_Trends";
 import HealthHome from "../../components/health/HealthHome";
@@ -19,53 +19,54 @@ export const DeanwoodHealth = ({resources}) => {
             el: <DeanwoodResident/>
         }
     ];
-    const [displayState, setDisplayState] = useState("healthHome");
-    return (<>
-        <Container className="city" id="deanwood-health">
-            <Row>
-                <Col md={3} sm={12} className="nav-col mr-2">
-                    <h1>Deanwood, D.C.</h1>
-                    <p>
-                        Deanwood lies in Ward 7 of the District of Columbia, home to over
-                        80 thousand (mostly Black) inhabitants. A couple of miles across the
-                        Anacostia lies Ward 3, with about the same number of (mostly White)
-                        inhabitants. However, the small distance between the two wards belies
-                        a world of difference. The disparity between Ward 3 and Ward 7 is not
-                        just demographic or economic: as shown here, it extends to people's
-                        health. Those who, by accident of birth, lie in the wrong side of the
-                        divide can expect to lead lives that are ten years shorter and have
-                        higher rates of chronic diseases such as diabetes.
-                    </p>
-                    <DeanwoodNav selected={"health"} resources={resources}/>
-                </Col>
-                <Row className="justify-content-around mt-5">
-                    <Col md={4} className={"p-0"}/>
-                    <Col md={8} className={"p-0"}>
-                        <ul className="list-inline">
-                            {tabs.map((tab, idx) =>
-                                <li className="list-inline-item" key={`li-tab-${idx}`}>
-                                    <Button
-                                        className={`btn-health-tab ${displayState === tab.label ? "selected" : ""}`}
-                                        key={idx}
-                                        onClick={() => setDisplayState(tab.label)}>
-                                        {tab.name}
-                                    </Button>
-                                </li>
-                            )}
-                        </ul>
-                        <Col md={12} className={"p-0"}>
-                            {tabs.map((tab, idx) =>
-                                <div key={idx}>
-                                    {displayState === tab.label && tab.el}
-                                </div>
-                            )}
-                        </Col>
-                    </Col>
-                </Row>
-            </Row>
-        </Container>
+    const [displayState, setDisplayState] = useState("covidData");
+    return (<Container className="city" id="deanwood-health">
+        <Row>
+            <Col md={3} xl={3} className="nav-col mr-2">
+                <h1>Deanwood, D.C.</h1>
+                <p>
+                    Deanwood lies in Ward 7 of the District of Columbia, home to over
+                    80 thousand (mostly Black) inhabitants. A couple of miles across the
+                    Anacostia lies Ward 3, with about the same number of (mostly White)
+                    inhabitants. However, the small distance between the two wards belies
+                    a world of difference. The disparity between Ward 3 and Ward 7 is not
+                    just demographic or economic: as shown here, it extends to people's
+                    health. Those who, by accident of birth, lie in the wrong side of the
+                    divide can expect to lead lives that are ten years shorter and have
+                    higher rates of chronic diseases such as diabetes.
+                </p>
+                <DeanwoodNav selected={"health"} resources={resources}/>
+            </Col>
+            <Row className="prime mt-5">
+                <Col md={4}/>
 
-    </>);
+                <Col md={8} className={"p-0"}>
+                    <ul className="list-inline">
+                        {tabs.map((tab, idx) =>
+                            <li className="list-inline-item" key={`li-tab-${idx}`}>
+                                <Button
+                                    className={`btn-health-tab ${displayState === tab.label ? "selected" : ""}`}
+                                    key={idx}
+                                    onClick={() => setDisplayState(tab.label)}>
+                                    {tab.name}
+                                </Button>
+                            </li>
+                        )}
+                    </ul>
+                </Col>
+            </Row>
+            <Row className="mt-3">
+                <Col md={3}/>
+                <Col md={9} className={"p-0"}>
+                    {tabs.map((tab, idx) =>
+                        <Row className={"p-0"} key={idx}>
+                            {displayState === tab.label && tab.el}
+                        </Row>
+                    )}
+                </Col>
+            </Row>
+        </Row>
+    </Container>);
 
 };
 
