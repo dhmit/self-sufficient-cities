4701ee47a9bb36442a7afd8804d846fd25ff518b
MasterEchez <32005788+MasterEchez@users.noreply.github.com>
2022-05-06 16:22:03 -0400

    Add sources to transport page

--- frontend/pages/deanwood/DeanwoodTransport.js
+++ frontend/pages/deanwood/DeanwoodTransport.js
@@ -44,6 +44,18 @@ export const DeanwoodTransport = ({resources}) => {
                               title={"Dwight D. Eisenhower Presidential Library - " +
                               "An excerpt from President Eisenhower’s Message to Congress " +
                               "regarding highways, February 22, 1955"}/>
+                    <Citation identifier={"source-2"}
+                              title={"The Interstate Highway System"}
+                              link={"https://www.history.com/topics/us-states/" +
+                              "interstate-highway-system#:~:text=The%20Federal%2DAid%20Highway%20" +
+                              "Act%20of%201956,-It%20took%20several&" +
+                              "text=The%20law%20authorized%20the%20construction,the%20" +
+                              "cost%20of%20expressway%20construction."}/>
+                    <Citation identifier={"source-3"}
+                              title={"Richard F. Weingroff, " + "Federal-Aid Highway Act of 1956:" +
+                              " Creating The Interstate System"}
+                              link={"https://highways.dot.gov/public-roads/summer-1996/" +
+                              "federal-aid-highway-act-1956-creating-interstate-system"}/>
                 </Col>
             </Row>
         </Container>);
