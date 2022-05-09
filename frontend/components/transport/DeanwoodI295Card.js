d6a6ff36faa31b45d04ad5a48244f11d57896de4
anastasia <anastasia.aizman@gmail.com>
2022-05-09 17:18:53 -0400

    update - transportation page

--- frontend/components/transport/DeanwoodI295Card.js
+++ frontend/components/transport/DeanwoodI295Card.js
@@ -9,10 +9,7 @@ const DeanwoodI295Card = ({deanwood_boundary, kenilworth_boundary}) => {
             <h3>I-295 and Deanwood</h3>
             <div>
                 <p>
-                    The story of I-295 and Deanwood begins with the trend of Urban Renewal in
-                    Washington D.C. before the post World War II period. Alleyways where
-                    African Americans lived were cleared under the guise of “sanitation
-                    reasons,” before they were pushed into Deanwood in the 1940s. From this
+                    From this
                     point there was the beginning of a “white flight” from Deanwood to other
                     areas, with Kenilworth being one of them. This marks the beginning of a
                     demographic shift of the Deanwood population, and one can see how the
@@ -21,6 +18,8 @@ const DeanwoodI295Card = ({deanwood_boundary, kenilworth_boundary}) => {
                     neighborhoods in Deanwood from gaining access to the parks on the west
                     side of the highway, which you can see from the map below.
                 </p>
+                The story of I-295 and Deanwood begins with the trend of Urban Renewal in
+                    Washington D.C. before World War II period.
             </div>
 
             <MapDeanwood deanwoodBoundary={deanwood_boundary}
---
+++ frontend/components/transport/Streets.js
@@ -0,0 +1,58 @@
+import React, {useState} from "react";
+import MapMarkers from "../maps/MapMarkers";
+import PropTypes from "prop-types";
+import {Image} from "react-bootstrap";
+
+export const Streets = ({position, places}) => {
+    const coordinates = places.map(p => {
+        return p.coordinates;
+    });
+
+    const fixTitle = (name) => {
+        return name.replaceAll("_", " ").toUpperCase();
+    };
+    const [image, setImage] = useState(places[0].image);
+    const [title, setTitle] = useState(fixTitle(places[0].name));
+    const [selected, setSelected] = useState(coordinates[0]);
+    const selectMarker = (obj) => {
+        let place = places.find(p => {
+            if (p.coordinates[0] === obj.latlng.lat && p.coordinates[1] === obj.latlng.lng) {
+                return p;
+            }
+        });
+        setSelected(place.coordinates);
+        setImage(place.image);
+        setTitle(fixTitle(place.name));
+    };
+
+
+    return (<>
+        <div className="carousel">
+            <div className="p-lg-3 p-sm-2 container">
+                <div className="p-0 row">
+                    <div className="p-0 col-6">
+                        <h5 className="tab-header">
+                            {title}
+                        </h5>
+                        <MapMarkers position={position}
+                                    selectMarker={selectMarker}
+                                    zoom={14}
+                                    markers={coordinates}
+                                    selected={selected}/>
+                    </div>
+                    <div className="pl-2 col-6 mt-4">
+                        <Image src={image} fluid={true}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>);
+};
+
+Streets.propTypes = {
+    position: PropTypes.array,
+    places: PropTypes.object
+};
+
+
+export default Streets;
